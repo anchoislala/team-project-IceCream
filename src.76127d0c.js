a750b46baf46d903af8d423d502599bbc1827f29
anchoislala <91604043+anchoislala@users.noreply.github.com>
2022-01-16 12:58:33 +0000

    Deploying to gh-pages from @ anchoislala/team-project-IceCream@51bc0322f67a329db02cba5869e86cae4a3ccf63 🚀

--- src.76127d0c.js
+++ src.76127d0c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-IceCream/src.b8b1f6f4.js.map
+//# sourceMappingURL=/team-project-IceCream/src.76127d0c.js.map
