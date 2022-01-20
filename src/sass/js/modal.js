92f94e59e6a7a1ded88a26831d523e52124b5547
Semen-Gomzyak <Blacklife1817@gmail.com>
2022-01-20 17:05:59 +0200

    location and franchise
    Локация и франшиза

--- src/sass/js/modal.js
+++ src/sass/js/modal.js
@@ -27,3 +27,34 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-location-open]'),
+      closeModalBtn: document.querySelector('[data-modal-location-close]'),
+      modal: document.querySelector('[data-modal-location]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[franchise-modal-open]'),
+      closeModalBtn: document.querySelector('[franchise-modal-close]'),
+      modal: document.querySelector('[franchise-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
